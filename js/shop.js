9ffd594fb47a092f5873b32f9e346efac91b1a99
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-04-25 12:59:13 +0200

    ex-9

--- js/shop.js
+++ js/shop.js
@@ -150,10 +150,12 @@ function addToCart(id) {
 }
 
 function cleanCart() {
+    
     while (cart.length > 0) {
-        cart.pop();
+        cart.pop();        
         console.log(cart);
     }
+    printCart(clean);
 }
 
 function calculateTotal(cart) {
@@ -175,7 +177,7 @@ function applyPromotionsCart(i) {
         cart[i].subtotal = cart[i].price * cart[i].quantity;
         cart[i].subtotalWithDiscount = (cart[i].price * 2 / 3).toFixed(3) * cart[i].quantity;
     } else {
-        cart[i].subtotalWithDiscount = "No Discount";
+        cart[i].subtotalWithDiscount = 0;
     }
 }
 
@@ -196,11 +198,32 @@ function removeFromCart(id) {
 }
 
 // Exercise 9
-function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+function printCart(clean) {
+
+    let text = "";
+    let i;
+    let totalCart = 0;
+
+    for (i = 0; i < cart.length; i++){  
+        text += "<li> <b>Product: </b>" + cart[i].name +  "<br>" +
+                " <b>Price: </b>" + cart[i].price + "<br>" +
+                " <b>Quantity: </b>" +cart[i].quantity + "<br>" +
+                " <b>Total: </b>" +cart[i].subtotal + "<br>" +
+                " <b>Total with discount: </b>" + cart[i].subtotalWithDiscount +"</li>";
+        totalCart += cart[i].subtotal 
+            if (cart[i].subtotalWithDiscount) {
+                totalCart = totalCart - cart[i].subtotal + cart[i].subtotalWithDiscount;
+            }
+
+        document.getElementById("list").innerHTML = text + "<br> <b>Total: </b>" + totalCart.toFixed(2);
+    }
+    if (clean) {
+        document.getElementById("list").innerHTML = "Cart cleaned!";
+    }
 }
 
 
 function open_modal() {
     console.log("Open Modal");
+    printCart();
 }
